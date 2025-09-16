6d257da96ced76af6cbafc5110e23bcbf39c8fb5
jiepan <jie.pan@intel.com>
2025-09-16 14:44:17 +0800

    add bench.sh

---
+++ bench.sh
@@ -0,0 +1,3 @@
+NODE=/home/panjie/tools/node/out/Release/node
+cd /home/panjie/tools/tesseract.js-core/examples/node/minimal; 
+${NODE}  ./index.wasm-simd.js  ../../../tests/unit/assets/images/testocr.png
